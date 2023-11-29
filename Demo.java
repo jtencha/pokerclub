aa8159e347dad1db2168c2845ef9f2d36e618ddc
Benjamin <dapperbird0@gmail.com>
2023-11-29 14:40:00 -0500

    11/29 test

---
+++ Demo.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Demo {
+    public static void main(String[] args) {
+        Poker poker1 = new Poker();
+        ArrayList<String> names = new ArrayList<String>();
+        names.add("H9");
+    }
+}
